364b134f6400c967ae2e11a4f5b1f2187b275681
boarder66 <35052750+boarder66@users.noreply.github.com>
2019-08-05 15:01:43 -0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,132 @@
+const QUESTIONANSWER = [
+
+	{
+		number: 0,
+		question: 'Who was the MVP of Super Bowl 1?',
+		answers: [
+			'Brett Favre',
+			'Frank Tarkentin',
+			'Bart Starr',
+			'John Riggins',
+			'Joe Theisman',
+		],
+		correctAnswer: 2, //'Bart Starr' red flag if coping same info across page
+	},
+
+	{
+		number: 1,
+		question: 'What city hosted the 1980 Summer Olympics?',
+		answers: [
+			'Moscow, Russia',
+			'Atlanta, New York',
+			'Mexico City, Mexico',
+			'Munich, Germany',
+			'Paris, France',
+		],
+		correctAnswer: 0,
+	},
+	
+	{
+		number: 2,
+		question: 'Who was the last professional hockey player to play without a helmet?',
+		answers: [
+			'Bobby Orr',
+			'Scott Stevens',
+			'Gordie Howe',
+			'Bobby Hull',
+			'Craig MacTavish',
+		],
+		correctAnswer: 4,
+	},
+
+	{
+		number: 3,
+		question: 'Grinding is to skateboarding as _______ is to snowboarding.',
+		answers: [
+			'Hydroplaning',
+			'Twerking',
+			'Sliding',
+			'Jibbing',
+			'Railing',
+		],
+		correctAnswer: 3,
+	},
+
+	{
+		number: 4,
+		question: 'In golf if someone told you the had and albatross today, what do they mean?',
+		answers: [
+			'They were 3-under par on a single hole.',
+			'They were 3-over par on a single hole.',
+			'They were 4-under par on a single hole.',
+			'They were 4-over par on a single hole.',
+			'They were 2-under par on a single hole.',
+		],
+		correctAnswer: 0,
+	},
+
+	{
+		number: 5,
+		question: 'In the MLB, what phrase became famous in the Barry Bonds steroid case?',
+		answers: [
+			'Andro and Test',
+			'D-Bol and the Clear',
+			'The Cream and Tren',
+			'The Cream and the Clear',
+			'HGH and Protein Shakes',
+		],
+		correctAnswer: 3,
+	},
+
+	{
+		number: 6,
+		question: 'What Nascar race is known as the \'Super Bowl of Racing\'?',
+		answers: [
+			'Southern 500 at Darlington',
+			'Coca-Cola 600 at Charlotte',
+			'Daytona 500',
+			'Brickyard 400 at Indianapolis',
+			'Championship Race at Homestead',
+		],
+		correctAnswer: 2,
+	},
+
+	{
+		number: 7,
+		question: 'What are the \'Big 3\' in powerlifting?',
+		answers: [
+			'Bench press, Deadlift, Squat',
+			'Bench press, Curls, Squat',
+			'Curls, Lunges, Pull-Ups',
+			'Squat, Deadlift, Rows',
+			'Pull-Ups, Squat, Push-Ups',
+		],
+		correctAnswer: 0,
+	},
+
+	{
+		number: 8,
+		question: 'In Bowling what is the term used when a player bowls 3 strikes in a row?',
+		answers: [
+			'Chicken',
+			'Turkey',
+			'Horse',
+			'Rooster',
+			'Eagle',
+		],
+		correctAnswer: 1,
+	},
+
+	{
+		number: 9,
+		question: 'Tom Brady, Michael Jordan, and Wayne Gretzky are also know as the _______ in their respective sport.',
+		answers: [
+			'DOG',
+			'GOLD',
+			'LVP',
+			'BOAT',
+			'GOAT',	
+		],
+		correctAnswer: 4,
+	},
+];
